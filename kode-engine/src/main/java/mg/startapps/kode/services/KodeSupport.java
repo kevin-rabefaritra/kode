a5dfd586e5a15c61df47c5023b974f5d7dfa8202
Kevin RABEFARITRA <rabefaritra.kevin@gmail.com>
2019-02-07 23:17:41 +0800

    - Migrated to AndroidX - Updated Build gradle version - Proguard

--- kode-engine/src/main/java/mg/startapps/kode/services/KodeSupport.java
+++ kode-engine/src/main/java/mg/startapps/kode/services/KodeSupport.java
@@ -3,8 +3,8 @@ package mg.startapps.kode.services;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.util.Log;
 
 import org.json.JSONObject;
